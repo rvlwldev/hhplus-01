9eb0cb3b11d8b0d1999a746893b8afab95e9a297
rvlwldev <hyungjun@gimhyeongjun-ui-noteubug.local>
2024-09-27 04:16:46 +0900

    feat: 동시성 제어를 위한 lockerMap 생성 및 충전/사용 메서드에 적용

--- src/main/kotlin/io/hhplus/tdd/point/PointService.kt
+++ src/main/kotlin/io/hhplus/tdd/point/PointService.kt
@@ -1,6 +1,9 @@
 package io.hhplus.tdd.point
 
 import org.springframework.stereotype.Service
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
 
 
 @Service
@@ -8,25 +11,32 @@ class PointService(
     private val repo: PointRepositoryImpl
 ) {
     private val validator = PointValidator()
+    private val lockerMap = ConcurrentHashMap<Long, ReentrantLock>()
 
     fun get(id: Long): UserPoint = repo.get(id)
 
     fun getHistory(id: Long): List<PointHistory> = repo.getHistories(id)
 
     fun charge(id: Long, amount: Long): UserPoint {
-        val userPoint = repo.get(id)
-        validator.validate(userPoint, amount, TransactionType.CHARGE)
-        userPoint.charge(amount)
+        val locker = lockerMap.computeIfAbsent(id) { ReentrantLock() }
+        locker.withLock {
+            val userPoint = repo.get(id)
+            validator.validate(userPoint, amount, TransactionType.CHARGE)
+            userPoint.charge(amount)
 
-        return repo.save(userPoint, amount, TransactionType.CHARGE)
+            return repo.save(userPoint, amount, TransactionType.CHARGE)
+        }
     }
 
     fun use(id: Long, amount: Long): UserPoint {
-        val userPoint = repo.get(id)
-        validator.validate(userPoint, amount, TransactionType.USE)
-        userPoint.use(amount)
+        val locker = lockerMap.computeIfAbsent(id) { ReentrantLock() }
+        locker.withLock {
+            val userPoint = repo.get(id)
+            validator.validate(userPoint, amount, TransactionType.USE)
+            userPoint.use(amount)
 
-        return repo.save(userPoint, amount, TransactionType.USE)
+            return repo.save(userPoint, amount, TransactionType.USE)
+        }
     }
 
 }
