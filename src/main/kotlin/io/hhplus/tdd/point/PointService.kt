8c9ca5cd66d8f4ef98168b67c122f2898a28d16e
rvlwldev <hyungjun@gimhyeongjun-ui-noteubug.local>
2024-09-26 23:30:20 +0900

    feat: 기본기능(조회, 이력조회, 충전, 사용)구현 및 저장(IPointRepository) 구현

--- src/main/kotlin/io/hhplus/tdd/point/PointService.kt
+++ src/main/kotlin/io/hhplus/tdd/point/PointService.kt
@@ -2,6 +2,28 @@ package io.hhplus.tdd.point
 
 import org.springframework.stereotype.Service
 
+
 @Service
-class PointService {
+class PointService(
+    private val repo: PointRepositoryImpl
+) {
+
+    fun get(id: Long): UserPoint = repo.get(id)
+
+    fun getHistory(id: Long): List<PointHistory> = repo.getHistories(id)
+
+    fun charge(id: Long, amount: Long): UserPoint {
+        val userPoint = repo.get(id)
+        userPoint.charge(amount)
+
+        return repo.save(userPoint, amount, TransactionType.CHARGE)
+    }
+
+    fun use(id: Long, amount: Long): UserPoint {
+        val userPoint = repo.get(id)
+        userPoint.use(amount)
+
+        return repo.save(userPoint, amount, TransactionType.USE)
+    }
+
 }
